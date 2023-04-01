b15e9658607fb7d74c853e12cedc1ed281ae6236
gladiolaola <gladiola.olaola@ukr.net>
2023-04-01 23:56:51 +0300

    refactor code

---
+++ AcidTrip4/Assets/Scripts/Dialogue/Dialogue.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+//Data Structure for holding dialogue information
+//Name = Name on header
+//Sentences = Text to display on screen
+//Sentences is for storing data for a single dialogue screen
+public class Dialogue {
+
+	public string name;
+
+	[TextArea(3, 10)]
+	//? Idk what this is
+	public string[] sentences;
+
+}
