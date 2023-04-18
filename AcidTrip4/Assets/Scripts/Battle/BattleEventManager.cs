e05aaa1b7e2056ea2db1e01b6b6c10d2cc25b889
gladiolaola <gladiola.olaola@ukr.net>
2023-04-18 23:56:51 +0300

    Improve performance of data export

---
+++ AcidTrip4/Assets/Scripts/Battle/BattleEventManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleEventManager : MonoBehaviour
+{
+    public delegate void Reveal();
+    public static event Reveal OnEnemyRevealSelect;
+    public static event Reveal OnEnemySelected;
+    public static event Reveal OnEnemySelectCancel;
+
+    public delegate void GUI();
+    public static event Reveal OnGUIUpdate;
+
+    // Start is called before the first frame update
+    public void revealEnemySelector()
+    {
+        
+        if(OnEnemyRevealSelect != null)
+        {
+            OnEnemyRevealSelect();
+        }
+        
+    }
+
+    public void cancelEnemySelector()
+    {
+
+        if (OnEnemySelectCancel != null)
+        {
+            OnEnemySelectCancel();
+        }
+
+    }
+
+
+    public void hideEnemySelector()
+    {
+        
+        if(OnEnemySelected != null)
+        {
+            OnEnemySelected();
+        }
+        
+    }
+
+
+    public void updateGUIS()
+    {
+        if(OnGUIUpdate != null){
+            OnGUIUpdate();
+        }
+    }
+
+
+
+}
