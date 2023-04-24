f4367179fcba81beb6aa082c58e41cb5dd9a9414
gladiolaola <gladiola.olaola@ukr.net>
2023-04-24 23:56:51 +0300

    add new unit tests

---
+++ AcidTrip4/Assets/Scripts/Audio/BackgroundMusic.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour
+{
+    // Plays a random music track
+
+    public string[] musicTracks;
+
+    void Start()
+    {
+        int randomIndex = Random.Range(0, musicTracks.Length);
+
+        AudioManager.instance.Play(musicTracks[randomIndex]);
+    }
+
+
+    
+
+    public void stopAllTracks()
+    {  
+        for(int i = 0; i < musicTracks.Length; i++)
+        {
+            AudioManager.instance.Stop(musicTracks[i]);
+        }
+
+    }
+
+}
