e8135177c69674309be7720df8fb9222a071eea6
gladiolaola <gladiola.olaola@ukr.net>
2024-02-28 23:56:51 +0200

    Update dependencies

---
+++ AcidTrip4/Assets/LoadDemo.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadDemo : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioManager.instance.Play("title");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void loadScene(string scene)
+    {
+        
+        //SceneManager.LoadScene(sceneNo);
+        StartCoroutine(transition(scene));
+    }
+
+    IEnumerator transition(string scene){
+
+        yield return new WaitForSeconds(1.5f);
+        AudioManager.instance.Stop("title");
+        SceneManager.LoadScene(scene);
+    }
+}
